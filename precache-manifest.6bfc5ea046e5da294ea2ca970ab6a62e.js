f1bf9b902d33d7e920f9e6118b7f1b40dfcd4cd2
grace-ko <gracekodes@gmail.com>
2018-12-31 23:09:41 -0800

    Updates

---
+++ precache-manifest.6bfc5ea046e5da294ea2ca970ab6a62e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8532c21bd7bbf1dd1b0c",
+    "url": "/bloc-jams-react/static/js/runtime~main.8532c21b.js"
+  },
+  {
+    "revision": "83c5e037751214fbbc03",
+    "url": "/bloc-jams-react/static/js/main.83c5e037.chunk.js"
+  },
+  {
+    "revision": "c0e5d3f58f26af403492",
+    "url": "/bloc-jams-react/static/js/1.c0e5d3f5.chunk.js"
+  },
+  {
+    "revision": "83c5e037751214fbbc03",
+    "url": "/bloc-jams-react/static/css/main.d1e02d37.chunk.css"
+  },
+  {
+    "revision": "732fb5bb09248a1b80a80586dd61d878",
+    "url": "/bloc-jams-react/index.html"
+  }
+];
